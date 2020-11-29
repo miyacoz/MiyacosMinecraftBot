e600e69fe07204870373b3c0d2da8b30e84c01ca
miyacoz <z@zeppel.biz>
2020-11-30 00:40:37 +0900

    changed a variable name

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,18 @@ if (!TOKEN) {
   throw new Error('No token found!')
 }
 
-const eris = Eris(TOKEN)
+const client = Eris(TOKEN)
 
-eris.on('ready', () => console.log('ready'))
+client.on('ready', () => console.log('ready'))
 
-eris.on('messageCreate', (msg: Message) => { // When a message is created
+client.on('messageCreate', (msg: Message) => { // When a message is created
     if (msg.content === '!ping') { // If the message content is '!ping'
-        eris.createMessage(msg.channel.id, 'Pong!')
+        client.createMessage(msg.channel.id, 'Pong!')
         // Send a message in the same channel with 'Pong!'
     } else if(msg.content === '!pong') { // Otherwise, if the message is '!pong'
-        eris.createMessage(msg.channel.id, 'Ping!')
+        client.createMessage(msg.channel.id, 'Ping!')
         // Respond with 'Ping!'
     }
 })
 
-eris.connect()
+client.connect()
