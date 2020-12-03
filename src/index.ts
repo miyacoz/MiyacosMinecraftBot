1c77ac663baaf69a7aa72a9e3bc4c7758aa79901
miyacoz <z@zeppel.biz>
2020-12-03 16:53:52 +0900

    methodchained

--- src/index.ts
+++ src/index.ts
@@ -23,24 +23,23 @@ const roles = new Collection(Role)
 
 const omitGuild = (record: any) => omit(record, 'guild')
 
-client.on('ready', () => {
-  const guild = client.guilds.find(({ name }) => /miyaco.*minecraft/i.test(name))
+client
+  .on('ready', () => {
+    const guild = client.guilds.find(({ name }) => /miyaco.*minecraft/i.test(name))
 
-  if (guild) {
-    guild.members.filter(({ user }) => !user.bot).forEach(member => members.add(member))
-    members.forEach(({ user }) => users.add(user))
-    guild.roles.forEach(role => roles.add(role))
-    console.log('ready', guild.name, members.map(omitGuild))
-    // console.log(users, roles.map(omitGuild))
-  }
-})
-
-client.on('messageCreate', (message: Message) => {
-  if (message.content === '!ping') {
-    client.createMessage(message.channel.id, 'Pong!')
-  } else if(message.content === '!pong') {
-    client.createMessage(message.channel.id, 'Ping!')
-  }
-})
-
-client.connect()
+    if (guild) {
+      guild.members.filter(({ user }) => !user.bot).forEach(member => members.add(member))
+      members.forEach(({ user }) => users.add(user))
+      guild.roles.forEach(role => roles.add(role))
+      console.log('ready', guild.name, members.map(omitGuild))
+      // console.log(users, roles.map(omitGuild))
+    }
+  })
+  .on('messageCreate', (message: Message) => {
+    if (message.content === '!ping') {
+      client.createMessage(message.channel.id, 'Pong!')
+    } else if(message.content === '!pong') {
+      client.createMessage(message.channel.id, 'Ping!')
+    }
+  })
+  .connect()
