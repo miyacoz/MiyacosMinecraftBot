c6b2c942aa05fe2413254822d1b0a0811a959d61
miyacoz <z@zeppel.biz>
2020-12-03 18:54:54 +0900

    refactored

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import Eris, {
   Collection,
+  Guild,
   Member,
   Message,
   Role,
@@ -15,6 +16,12 @@ if (!TOKEN) {
   throw new Error('No token found!')
 }
 
+const BOT_ID = Config.CLIENT_ID
+
+if (!BOT_ID) {
+  throw new Error('No client id found!')
+}
+
 const client = Eris(TOKEN)
 
 const members = new Collection(Member)
@@ -23,23 +30,45 @@ const roles = new Collection(Role)
 
 const omitGuild = (record: any) => omit(record, 'guild')
 
-client
-  .on('ready', () => {
-    const guild = client.guilds.find(({ name }) => /miyaco.*minecraft/i.test(name))
+const updateState = (guild: Guild) => {
+  guild.members.filter(({ user }) => !user.bot).forEach(member => members.add(member))
+  members.forEach(({ user }) => users.add(user))
+  guild.roles.forEach(role => roles.add(role))
+  console.log('ready', guild.name, members.map(omitGuild))
+  // console.log(users, roles.map(omitGuild))
+}
 
-    if (guild) {
-      guild.members.filter(({ user }) => !user.bot).forEach(member => members.add(member))
-      members.forEach(({ user }) => users.add(user))
-      guild.roles.forEach(role => roles.add(role))
-      console.log('ready', guild.name, members.map(omitGuild))
-      // console.log(users, roles.map(omitGuild))
-    }
-  })
+const findGuildAndUpdateState = () => {
+  const guild = client.guilds.find(({ name }) => /miyaco.*minecraft/i.test(name))
+
+  if (guild) {
+    updateState(guild)
+  }
+}
+
+client
+  .on('ready', findGuildAndUpdateState)
+  .on('guildMemberRemove', updateState)
+  .on('guildMemberUpdate', updateState)
+  .on('guildRoleDelete', updateState)
+  .on('guildRoleUpdate', updateState)
   .on('messageCreate', (message: Message) => {
-    if (message.content === '!ping') {
-      client.createMessage(message.channel.id, 'Pong!')
-    } else if(message.content === '!pong') {
-      client.createMessage(message.channel.id, 'Ping!')
+    findGuildAndUpdateState()
+
+    if (message.mentions.some(user => user.id === BOT_ID)) {
+      const sanitisedMessage = message.content.replace(new RegExp(`<@!${BOT_ID}>`), '').trim().toLowerCase()
+      console.log(sanitisedMessage)
+
+      // inline replies https://github.com/abalabahaha/eris/issues/1084
+      if (sanitisedMessage === 'ping') {
+        client.createMessage(message.channel.id, 'Pong!')
+      } else if (sanitisedMessage === 'pong') {
+        client.createMessage(message.channel.id, 'Ping!')
+      } else {
+        client.createMessage(message.channel.id, '?')
+      }
     }
   })
+  .on('messageDelete', findGuildAndUpdateState)
+  .on('messageUpdate', findGuildAndUpdateState)
   .connect()
