3e53a9f6e9b63a3ead99474511932362d5f2085e
miyacoz <z@zeppel.biz>
2020-12-03 16:52:40 +0900

    made members/users/roles permanent and updatable

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import Eris, { Message } from 'eris'
+import Eris, {
+  Collection,
+  Member,
+  Message,
+  Role,
+  User,
+} from 'eris'
 import { omit } from 'lodash'
 
 import Config from './Config'
@@ -11,13 +17,21 @@ if (!TOKEN) {
 
 const client = Eris(TOKEN)
 
+const members = new Collection(Member)
+const users = new Collection(User)
+const roles = new Collection(Role)
+
+const omitGuild = (record: any) => omit(record, 'guild')
+
 client.on('ready', () => {
   const guild = client.guilds.find(({ name }) => /miyaco.*minecraft/i.test(name))
 
   if (guild) {
-    const members = guild.members.filter(({ user }) => !user.bot).map(user => omit(user, ['guild', 'user']))
-    const roles = guild.roles.map(role => omit(role, 'guild'))
-    console.log('ready', guild.name, members, roles)
+    guild.members.filter(({ user }) => !user.bot).forEach(member => members.add(member))
+    members.forEach(({ user }) => users.add(user))
+    guild.roles.forEach(role => roles.add(role))
+    console.log('ready', guild.name, members.map(omitGuild))
+    // console.log(users, roles.map(omitGuild))
   }
 })
 
