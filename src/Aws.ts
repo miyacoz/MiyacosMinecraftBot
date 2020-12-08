cb4e6eb80882c3ec049be09da22111c585af0962
miyacoz <z@zeppel.biz>
2020-12-09 00:49:47 +0900

    refactored

--- src/Aws.ts
+++ src/Aws.ts
@@ -1,16 +1,46 @@
-import { EC2 } from 'aws-sdk'
+import {
+  EC2,
+  Credentials,
+} from 'aws-sdk'
 
-export class Ec2Instance {
+import Config from './Config'
+
+type TState = 'pending' | 'running' | 'shutting-down' | 'terminated' | 'stopping' | 'stopped' | ''
+
+const credentials = new Credentials({
+  accessKeyId: Config.AWS_ACCESS_KEY_ID,
+  secretAccessKey: Config.AWS_SECRET_ACCESS_KEY,
+})
+
+const client = new EC2({
+  credentials,
+  region: 'ap-northeast-1',
+  apiVersion: '2016-11-15',
+  sslEnabled: true,
+});
+
+class Ec2Instance {
+  private _client: EC2
   private _id: string = ''
-  private _name: string = ''
+  private _name: string
   private _type: string = ''
-  private _state: string = ''
+  private _state: TState = ''
   private _stateReason: string = ''
   private _launchedAt: Date = new Date(0)
+  private _searchFilter: object
 
-  public constructor(data?: EC2.Instance) {
-    if (data) {
-      this.setData(data)
+  public constructor(client: EC2, keyName: string) {
+    this._client = client
+    this._name = keyName
+    this._searchFilter = {
+      Filters: [
+        {
+          Name: 'key-name',
+          Values: [
+            this.name,
+          ],
+        },
+      ],
     }
   }
 
@@ -18,7 +48,7 @@ export class Ec2Instance {
     this._id = data.InstanceId || ''
     this._name = data.KeyName || ''
     this._type = data.InstanceType || ''
-    this._state = data.State?.Name || ''
+    this._state = (data.State?.Name as TState) || ''
     this._stateReason = data.StateReason?.Code || ''
     this._launchedAt = data.LaunchTime || new Date(0)
   }
@@ -35,7 +65,7 @@ export class Ec2Instance {
     return this._type
   }
 
-  public get state(): string {
+  public get state(): TState {
     return this._state
   }
 
@@ -46,4 +76,27 @@ export class Ec2Instance {
   public get launchedAt(): Date {
     return this._launchedAt
   }
+
+  public async getInfo(): Promise<this> {
+    try {
+      const result = await this._client.describeInstances(this._searchFilter).promise()
+
+      const i = result.Reservations?.[0]?.Instances?.[0] || null
+
+      if (!i) {
+        throw new Error('No instance found!')
+      }
+
+      this.setData(i)
+
+      return this
+    } catch (e) {
+      console.warn(e)
+      throw new Error(e)
+    }
+  }
 }
+
+const ec2Instance = new Ec2Instance(client, Config.EC2_INSTANCE_KEY_NAME)
+
+export default ec2Instance
--- src/index.ts
+++ src/index.ts
@@ -7,53 +7,11 @@ import Eris, {
   User,
 } from 'eris'
 import { omit } from 'lodash'
-import {
-  EC2,
-  Credentials,
-} from 'aws-sdk'
 
 import Config from './Config'
-import { Ec2Instance } from './Aws'
+import Instance from './Aws'
 
-const credentials = new Credentials({
-  accessKeyId: Config.AWS_ACCESS_KEY_ID,
-  secretAccessKey: Config.AWS_SECRET_ACCESS_KEY,
-})
-
-const ec2 = new EC2({
-  credentials,
-  region: 'ap-northeast-1',
-  apiVersion: '2016-11-15',
-  sslEnabled: true,
-});
-
-const ec2Instance = new Ec2Instance()
-
-;(async () => {
-  try {
-    const result = await ec2.describeInstances({
-      Filters: [
-        {
-          Name: 'key-name',
-          Values: [
-            Config.EC2_INSTANCE_KEY_NAME,
-          ],
-        },
-      ],
-    }).promise()
-
-    const i = result.Reservations?.[0]?.Instances?.[0] || null
-
-    if (!i) {
-      throw new Error('No instance found!')
-    }
-
-    ec2Instance.setData(i)
-  } catch (e) {
-    console.warn(e)
-    throw new Error(e)
-  }
-})()
+Instance.getInfo()
 
 const client = Eris(Config.TOKEN)
 
@@ -77,34 +35,37 @@ const findGuildAndUpdateState = () => {
   }
 }
 
-const reply = (client: Client, message: Message, content: string) =>
+const post = (client: Client, message: Message, content: string, reply?: boolean): Promise<Message> =>
   // FIXME: restore eris version to dev/master
   client.createMessage(message.channel.id, {
     content,
     allowedMentions: {
       repliedUser: true,
     },
-    messageReferenceID: message.id,
+    messageReferenceID: reply ? message.id : undefined,
   })
 
 client
-  .on('ready', () => {
-    findGuildAndUpdateState()
-    console.log(ec2Instance)
-  })
+  .on('ready', findGuildAndUpdateState)
   .on('guildMemberRemove', updateState)
   .on('guildMemberUpdate', updateState)
   .on('guildRoleDelete', updateState)
   .on('guildRoleUpdate', updateState)
-  .on('messageCreate', (message: Message) => {
+  .on('messageCreate', async (message: Message) => {
     findGuildAndUpdateState()
 
     if (message.mentions.some(user => user.id === Config.CLIENT_ID)) {
-      const r = (content: string) => reply(client, message, content)
+      client.sendChannelTyping(message.channel.id)
+
+      const r = (content: string) => post(client, message, content, true)
 
       const sanitisedMessage = message.content.replace(new RegExp(`<@!${Config.CLIENT_ID}>`), '').trim().toLowerCase()
 
-      if (sanitisedMessage === 'ping') {
+      // not using switch because some complex condition might be needed
+      if (sanitisedMessage === 'status') {
+        const result = await Instance.getInfo()
+        await r(`The server is now ${result.state.replace('-', ' ')}.`)
+      } else if (sanitisedMessage === 'ping') {
         r('Pong!')
       } else if (sanitisedMessage === 'pong') {
         r('Ping!')
